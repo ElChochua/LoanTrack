b1d9c4306f16e51c1772101ab4c90c3e1a35c4c0
ElChochua <jossue.avll@gmail.com>
2024-09-29 02:44:32 -0700

    editar SideBar y agregar componentes faltantes

---
+++ src/app/shared/side-bar/side-bar.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'SideBar',
+  standalone: true,
+  imports: [],
+  templateUrl: './side-bar.component.html',
+  styleUrl: './side-bar.component.css'
+})
+export class SideBarComponent {
+
+}
